b7efb456bfc159ff8adcfb5aa5e70827e64d051e
Daniel Blagy <daniel.blagy@gmail.com>
2022-10-27 15:50:39 +0300

    set up db connection

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
 	fmt.Println("wb l0-blagy")
+
+	log.Print("Connecting to the db...")
+
+	dsn := "host=localhost user=l0user password=l0pass dbname=l0db port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to the database\n", err)
+	}
+
+	log.Print("Connected to the database\n", db)
 }
